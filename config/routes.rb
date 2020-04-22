982096b90193407050cb78fbca3505dc0a99ef1a
arebeb <romakislitsin89@gmail.com>
2020-04-22 19:38:49 +0600

    fix routes, add users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :currencies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :currencies, only: [:index, :show]
+  resources :users, param: :_username
 end
