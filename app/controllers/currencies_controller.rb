cea9d749cb4a086aa42f56ff70efc69fc8685f90
arebeb <romakislitsin89@gmail.com>
2020-04-22 19:34:02 +0600

    create Currency, run migration

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,22 @@
+class CurrenciesController < ApplicationController
+  before_action :set_currency, only: :show
+
+  # GET /currencies
+  def index
+    @currencies = Currency.all
+    render json: @currencies
+  end
+
+  # GET /currencies/1
+  def show
+    render json: @currency
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_currency
+    @currency = Currency.find(params[:id])
+  end
+
+end
